c5a0cabefe66e157f73e65c21e008f4e20b580b3
Wedi-Nachew <131978339+Wedi-Nachew@users.noreply.github.com>
2025-07-01 15:46:36 +0300

    updated build

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const Note = require("./models/note");
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(express.json());
 app.use(express.static("dist"));
+app.use(express.json());
 app.use(cors());
+
+const requestHandler = (req, res, next) => {
+    console.log("Method: ", req.method);
+    console.log("Path: ", req.path);
+    req.body && console.log("Body: ", req.body);
+    console.log("----------------");
+    next();
+};
+
+app.use(requestHandler);
 
 let notes = [
     {
@@ -34,51 +44,76 @@ let notes = [
 ];
 
 app.get("/", (req, res) => res.send("<h1>Welcome to our server</h1>"));
-app.get("/api/notes/:id", (req, res) => {
-    console.log("id: ", req.params.id);
-    Note.findById(req.params.id).then((note) => res.json(note));
-    // .then((note) => {
-    //     if (note) {
-    //         res.json(note);
-    //     } else {
-    //         res.status(404).end();
-    //     }
-    // })
-    // .catch((error) => {
-    //     res.status(400).json({ error: "malformatted id" });
-    // });
+app.get("/api/notes/:id", (req, res, next) => {
+    Note.findById(req.params.id)
+        .then((note) => {
+            if (note) {
+                res.json(note);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch((error) => {
+            next(error);
+        });
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-    const id = req.params.id;
-    notes = notes.filter((note) => note.id !== id);
-    res.status(204).end();
+app.delete("/api/notes/:id", (req, res, next) => {
+    Note.findByIdAndDelete(req.params.id)
+        .then((result) => res.status(204).end())
+        .catch((error) => next(error));
 });
 
 app.get("/api/notes", (req, res) => {
     Note.find({}).then((notes) => res.json(notes));
 });
 
-const generateId = () => {
-    const maxId =
-        notes.length > 0
-            ? Math.max(...notes.map((note) => Number(note.id)))
-            : 0;
-    return String(maxId + 1);
-};
-
-app.post("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res, next) => {
     const body = req.body;
-    if (!body.content) {
-        return res.status(400).json({ error: "content missing" });
-    }
 
     const note = new Note({
         content: body.content,
         important: body.important || false,
     });
-    note.save().then((savedNote) => res.json(savedNote));
+    note.save()
+        .then((savedNote) => res.json(savedNote))
+        .catch((error) => next(error));
 });
+
+app.put("/api/notes/:id", (req, res, next) => {
+    const { content, important } = req.body;
+
+    Note.findById(req.params.id)
+        .then((note) => {
+            if (!note) {
+                return res.status(404).end();
+            }
+
+            note.content = content;
+            note.important = important;
+
+            return note.save().then((updatedNote) => res.json(updatedNote));
+        })
+        .catch((error) => next(error));
+});
+const unkownEndpoint = (req, res, next) => {
+    res.status(404).send({ error: "Unknown endpoint" });
+};
+
+app.use(unkownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+    console.log(error.name);
+    if (error.name === "CastError") {
+        return res.status(400).send({ error: "malformatted id" });
+    } else if (error.name === "ValidationError") {
+        return res.status(400).send({ error: error.message });
+    }
+
+    next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- models/note.js
+++ models/note.js
@@ -11,7 +11,14 @@ mongoose
         console.log("error connecting to MongoDB", error.message)
     );
 
-const noteSchema = new mongoose.Schema({ content: String, important: Boolean });
+const noteSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        minLength: 5,
+        required: true,
+    },
+    important: Boolean,
+});
 noteSchema.set("toJSON", {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString();
